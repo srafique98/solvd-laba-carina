e268ede2b4613b866571cc8e764e4261850c4dbc
srafique98 <shanrafique98@gmail.com>
2024-02-06 23:24:03 -0800

    notes for carina

---
+++ src/main/java/com/solvd/carinaexample/api/GetUserByUsername.java
@@ -0,0 +1,20 @@
+package com.solvd.carinaexample.api;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.utils.config.Configuration;
+
+@Endpoint(url = "${config.api_url}/users/${username}", methodType = HttpMethodType.GET) // Get request
+@ResponseTemplatePath(path = "api/users/get_user_rs.json") // post put patch request with body
+public class GetUserByUsername extends AbstractApiMethodV2 {
+    public GetUserByUsername(String username) {
+//        super(null,"api/users/get_user_rs.json"); // don't need this because @ResponseTemplatePath line 11 and addProperty in UserTest.java
+//        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        replaceUrlPlaceholder("username", username);
+
+        ignorePropertiesProcessor(NotStringValuesProcessor.class);
+    }
+}
---
+++ src/test/java/com/solvd/carinaexample/UserTest.java
@@ -0,0 +1,66 @@
+package com.solvd.carinaexample;
+
+import com.solvd.carinaexample.api.GetUserByUsername;
+import com.solvd.carinaexample.domain.User;
+import com.zebrunner.carina.api.apitools.validation.JsonComparatorContext;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+
+public class UserTest {
+    @DataProvider(name = "userNames")
+    public Object[][] userNames() {
+        return new Object[][] {
+                {"brutskov","Bahdan","Rutskou","Solvd Inc."}, // has to be exactly like json values b4 putting ${user.company}
+//                {"v", "blah", "nope","none"}
+        };
+    }
+
+    // after running verifyGetUserByUserNameTest method
+    // we have no body for GET request -- Body:			<none>
+    // many headers for response
+    // & have response body
+    @Test(description = "verifies users", dataProvider = "userNames")
+    public void verifyGetUserByUserNameTest(String userName, String firstName,String lastName, String company){
+        User user = new User();
+        user.setUsername(userName);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setCompany(company);
+        GetUserByUsername getUserByUsername = new GetUserByUsername(user.getUsername());
+
+        getUserByUsername.addProperty("user", user); // user.firstName in get_user_rs.json -- key is user
+                                                    // value is object
+        // property is last parameter for super(null,"src/test/resources/api/users/get_user_rs.json"); in GetUserByUsername class
+
+        // each response has http code and we are checking if response was successful
+        getUserByUsername.expectResponseStatus(HttpResponseStatusType.OK_200);
+        //sending request and validate response
+        getUserByUsername.callAPI();
+
+        JsonComparatorContext comparatorContext = JsonComparatorContext.context()
+                .<String>withPredicate("datePredicate", date -> isDateValid(date) &&
+                        ZonedDateTime.parse(date) // "2024-01-25T10:35:32Z" has Z at end so using ZonedDateTime
+                                .isAfter(LocalDate.of(2000,1,1)
+                                        .atStartOfDay(ZoneId.systemDefault())));
+                                    // .withPredicate is generic.. check by command then click
+
+        // blank is fine but we want to use predicate -- "updated_at": "predicate:datePredicate" (in json file)
+        getUserByUsername.validateResponse(comparatorContext);
+
+    }
+
+    private static boolean isDateValid(String date){
+        try {
+            ZonedDateTime.parse(date);
+            return true;
+        }catch (DateTimeParseException e){
+            return false;
+        }
+    }
+}
